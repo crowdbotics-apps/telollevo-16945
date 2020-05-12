9489009ebe5a3f079bb7f47dca935dc0803f4a0f
Crowdbotics <team@crowdbotics.com>
2020-05-12 21:21:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/telollevo_16945/MainActivity.java
@@ -0,0 +1,15 @@
+package com.telollevo_16945;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "telollevo_16945";
+    }
+}
